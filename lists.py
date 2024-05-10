dad6fa8a14fbd3c25b89ed424ce3618737d63c89
Marco-Battistoni <162721472+Marco-Battistoni@users.noreply.github.com>
2024-05-10 11:45:11 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,40 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) >= 6:
+        del list_to_remove_elements[5]
+    if len(list_to_remove_elements) >= 5:
+        del list_to_remove_elements[4]
+    if len(list_to_remove_elements) >=1:
+        del list_to_remove_elements[0] 
+    return list_to_remove_elements
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.insert(0, "Pink")
+    a = len(list_to_add_elements)+1
+    list_to_add_elements.insert(a, "Yellow")
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check) == 0:
+        return True
+    else:
+        return False
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >=3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
+    else: 
+        return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    a = list_of_lists_to_modify[0][0:2]
+    b = list_of_lists_to_modify[1][1:4]
+    c = list_of_lists_to_modify[2][-2:]
+    list_of_lists_to_modify = [a,b,c]
+    return list_of_lists_to_modify
